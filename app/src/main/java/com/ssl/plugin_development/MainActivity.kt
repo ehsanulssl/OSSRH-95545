ed6070c4b2cb2dfdbb3b2ada5adc585ba9ebf895
Md Ehsnaul Hoque <ehsanul.hoque@sslwireless.com>
2023-10-04 13:17:56 +0600

    Plugin Developed

--- app/src/main/java/com/ssl/plugin_development/MainActivity.kt
+++ app/src/main/java/com/ssl/plugin_development/MainActivity.kt
@@ -24,6 +24,7 @@ import com.sslwireless.sslcommerzlibrary.view.singleton.IntegrateSSLCommerz
 import com.sslwireless.sslcommerzlibrary.viewmodel.listener.SSLCTransactionResponseListener
 
 
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
